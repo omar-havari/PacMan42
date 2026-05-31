c7c82c8be40d261494bfbf5d92ff23166b5e7040
omar-havari <omaravari12@gmail.com>
2026-05-31 22:39:27 +0200

    Structured the code a bit more

--- main_menu_UI.py
+++ main_menu_UI.py
@@ -4,20 +4,37 @@ import sys
 # Initializing pygame
 pygame.init()
 
-# 1. Get dimension of screen
+# Get dimension of screen
 info = pygame.display.Info()
 screen_width = info.current_w
 screen_height = info.current_h
 
-# 2. Pass the FULLSCREEN flag for true fullscreen
+# Pass the FULLSCREEN flag for true fullscreen
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 pygame.display.set_caption("Test")
 
-font = pygame.font.Font("PressStart2P-Regular.ttf", 300)
-main_title = font.render("Pacman", False, (255, 255, 0))
+# Creating font for main title and main title text
+font_main = pygame.font.Font("PressStart2P-Regular.ttf", 300)
+main_title = font_main.render("Pacman", False, (255, 255, 0))
 
+
+# Creating font for buttons and the texts which will serve as buttons
+font_menu_buttons = pygame.font.Font("PressStart2P-Regular.ttf", 100)
+start_game = font_menu_buttons.render("New Game", False, (255, 255, 0))
+high_scores = font_menu_buttons.render("High scores", False, (255, 255, 0))
+
+# Applying dimensions
 screen_width, screen_height = screen.get_size()
-text_box = main_title.get_rect(center = (screen_width / 2, screen_height / 2 - 500))
+main_title_box = main_title.get_rect(center = (screen_width / 2, screen_height / 2 - 500))
+start_game_box = start_game.get_rect(center = (screen_width / 2, screen_height / 2 ))
+high_scores_box = high_scores.get_rect(center = (screen_width / 2, screen_height / 2 + 180))
+
+# Drawing main title and buttons
+screen.fill((0, 0, 0))
+screen.blit(main_title, main_title_box)
+screen.blit(start_game, start_game_box)
+screen.blit(high_scores, high_scores_box)
+pygame.display.flip()
 
 # Game loop
 running = True
@@ -31,10 +48,6 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
-        
-        screen.fill((0, 0, 0))
-        screen.blit(main_title, text_box)
-        pygame.display.flip()
 
 # Quit pygame
 pygame.quit()
